ed71b0a7a4a15befeb0005e9d235c8e487126c4e
laura-bogaard <ltb1@st-andrews.ac.uk>
2021-08-02 14:51:23 +0100

    save

--- DISTANCE GLM.R
+++ DISTANCE GLM.R
@@ -16,46 +16,52 @@ names(distdat)
 
 
 #start with full model
-m1 <- glm(tide_dist ~ treatment + rlmid_emp + jul_day + hour 
-          + foraging + obs + tide_ht + fish, na.action = "na.fail",
+m1 <- glm(tide_dist ~ treatment + s(rlmid_emp) + s(jul_day) + hour 
+          + foraging + tide_ht + fish, na.action = "na.fail",
           family = "poisson", offset = log(dur_hr), data = distdat)
-chat <- deviance(m1) / df.residual(m1)
 
+chat <- deviance(m1) / df.residual(m1)
+# hack dredge function by fitting a poisson model as m1 and then selecting QAIC
 dredge(m1, rank = "QAIC", chat = chat, evaluate = T, fixed = c("treatment"))
+
+# best models are tied: 
+# 1: julday, obs, rlmemp, treat
+# 2: hour, julday, rlmemp treat
+
+
+# but what if i kickout observer from the beginning, then the other is still the top mod
+# interesting if I try a gam with a smooth for rl and day... 
+
+
 summary(m1)
 
 plot(m1)
 #kick out observer
 m2 <- glm(tide_dist ~ treatment + rlmid_emp + jul_day + hour 
-                + foraging + tide_ht + fish,family = "poisson", offset = log(dur_hr), data = distdat)
+                + foraging + tide_ht + fish,family = "quasipoisson", offset = log(dur_hr), data = distdat)
 
 summary(m2)
 # kick out tide height
 m3 <- glm(tide_dist ~ treatment + rlmid_emp + jul_day + hour 
-          + foraging + fish, family = "poisson", offset = log(dur_hr), data = distdat)
+          + foraging + fish, family = "quasipoisson", offset = log(dur_hr), data = distdat)
 
 summary(m3)
 
 #kickout foraging
 m4 <- glm(tide_dist ~ treatment + rlmid_emp + jul_day + hour 
-          + fish, family = "poisson", offset = log(dur_hr), data = distdat)
+          + fish, family = "quasipoisson", offset = log(dur_hr), data = distdat)
 
 summary(m4)
 
 #kickout fish
-m5 <- glm(tide_dist ~ treatment + rlmid_emp + jul_day + hour, offset = log(dur_hr), family = "poisson", data = distdat)
+m5 <- glm(tide_dist ~ treatment + rlmid_emp + jul_day + hour, offset = log(dur_hr), family = "quasipoisson", data = distdat)
 
 summary(m5)
 
 #kickout hour
-m6 <- glm(tide_dist ~ treatment + rlmid_emp + jul_day, offset = log(dur_hr), family = "poisson", data = distdat)
+m6 <- glm(tide_dist ~ treatment + rlmid_emp + jul_day, offset = log(dur_hr), family = "quasipoisson", data = distdat)
 
 summary(m6)
 
 
-
-
-
-QAIC(m1, m2, m3, m4, m5, m6, chat = chat)
-?QAIC
 
